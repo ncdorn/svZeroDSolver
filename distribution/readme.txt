c6d539b258cf3adf269821a2875fb417927dd048
ncdorn <ndorn@stanford.edu>
2024-11-15 15:11:12 -0800

    sync vs code bug

--- distribution/readme.txt
+++
@@ -1,1 +0,0 @@
-svZeroDSolver is a fast lumped-parameter solver for blood flow and pressure in hemodynamic networks.
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from cmake_setuptools import CMakeExtension, CMakeBuildExt
-
-setup(
-    ext_modules=[CMakeExtension("pysvzerod")],
-    cmdclass={"build_ext": CMakeBuildExt},
-)
